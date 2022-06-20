19b9aa72175a86f441c4c993a6057781907f518f
sabinbaniya <dummymail@tutanota.de>
2022-06-20 15:51:22 +0545

    initial commit

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pixel IT Magic Solutions</title>
+    <link rel="stylesheet" href="./assets/styles/style.css">
+</head>
+
+<body class="max-w-[90%] mx-auto">
+
+    <section class="my-12">
+        <h1 class="text-7xl font-bold leading-[80px]">Courses for</br> Everyone</h1>
+    </section>
+
+</body>
+
+</html>
