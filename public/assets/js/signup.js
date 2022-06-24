c7ab2c66e6dd2a337f25e7f83931c0f4775c2252
sabinbaniya <dummymail@tutanota.de>
2022-06-24 13:25:33 +0545

    Client side validation for signup page

---
+++ public/assets/js/signup.js
@@ -0,0 +1,230 @@
+//select all input fields
+const name = document.querySelector("#name");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const email = document.querySelector("#email");
+const number = document.querySelector("#number");
+const birthdate = document.querySelector("#birth-date");
+const gender = document.querySelector("#gender");
+const form = document.querySelector("#signup_form");
+
+// select all error messages
+const name_error = document.querySelector("#name_error");
+const username_error = document.querySelector("#username_error");
+const password_error = document.querySelector("#password_error");
+const email_error = document.querySelector("#email_error");
+const number_error = document.querySelector("#number_error");
+const birthdate_error = document.querySelector("#birth-date_error");
+const gender_error = document.querySelector("#gender_error");
+
+// select all icons
+const name_icon = document.querySelector("#name_icon");
+const username_icon = document.querySelector("#username_icon");
+const password_icon = document.querySelector("#password_icon");
+const email_icon = document.querySelector("#email_icon");
+const number_icon = document.querySelector("#number_icon");
+const birthdate_icon = document.querySelector("#birth-date_icon");
+const gender_icon = document.querySelector("#gender_icon");
+
+// set date's select fields to be atleast 8 years back from todays date
+const todaysDate = new Date().toLocaleDateString();
+const year = parseInt(todaysDate.split("/")[2]) - 8;
+const month =
+  todaysDate.split("/")[0].length === 1
+    ? "0" + todaysDate.split("/")[0]
+    : todaysDate.split("/")[0];
+const day =
+  todaysDate.split("/")[1].length === 1
+    ? "0" + todaysDate.split("/")[1]
+    : todaysDate.split("/")[1];
+const allowedDate = [year, month, day].join("-");
+
+birthdate.max = allowedDate;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //reset all error messasges on submit
+  name_error.classList.add("hidden");
+  username_error.classList.add("hidden");
+  password_error.classList.add("hidden");
+  email_error.classList.add("hidden");
+  number_error.classList.add("hidden");
+  birthdate_error.classList.add("hidden");
+  gender_error.classList.add("hidden");
+  name.classList.remove("invalid_input");
+  username.classList.remove("invalid_input");
+  password.classList.remove("invalid_input");
+  email.classList.remove("invalid_input");
+  number.classList.remove("invalid_input");
+  birthdate.classList.remove("invalid_input");
+  gender.classList.remove("invalid_input");
+  name_icon.classList.remove("invalid_input_icon");
+  username_icon.classList.remove("invalid_input_icon");
+  password_icon.classList.remove("invalid_input_icon");
+  email_icon.classList.remove("invalid_input_icon");
+  number_icon.classList.remove("invalid_input_icon");
+  birthdate_icon.classList.remove("invalid_input_icon");
+  gender_icon.classList.remove("invalid_input_icon");
+  name_icon.classList.add("valid_input_icon");
+  username_icon.classList.add("valid_input_icon");
+  password_icon.classList.add("valid_input_icon");
+  email_icon.classList.add("valid_input_icon");
+  number_icon.classList.add("valid_input_icon");
+  birthdate_icon.classList.add("valid_input_icon");
+  gender_icon.classList.add("valid_input_icon");
+
+  const name_val = document.querySelector("#name").value;
+  const username_val = document.querySelector("#username").value;
+  const password_val = document.querySelector("#password").value;
+  const email_val = document.querySelector("#email").value;
+  const number_val = document.querySelector("#number").value;
+  const birthdate_val = document.querySelector("#birth-date").value;
+  let errors_count = 0;
+
+  // name's length check
+  if (!name_val) {
+    name_error.innerText = "Please enter your name";
+    name_error.classList.remove("hidden");
+    name.classList.add("invalid_input");
+    name_icon.classList.add("invalid_input_icon");
+    errors_count++;
+  } else if (name_val.length > 20) {
+    name_error.innerText = "Name cannot be longer than 20 characters";
+    name_error.classList.remove("hidden");
+    name.classList.add("invalid_input");
+    name_icon.classList.add("invalid_input_icon");
+    errors_count++;
+  }
+
+  // name's pattern test, throws error when user gives, special chars or numbers on name field
+  const name_pattern = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~|0-9]/;
+  if (name_pattern.test(name_val)) {
+    name_error.innerText = "Please enter your real name";
+    name_error.classList.remove("hidden");
+    name.classList.add("invalid_input");
+    name_icon.classList.add("invalid_input_icon");
+    errors_count++;
+  }
+
+  // username's length check
+  if (!username_val) {
+    username_error.innerText = "Please enter your username";
+    username_error.classList.remove("hidden");
+    errors_count++;
+    username.classList.add("invalid_input");
+    username_icon.classList.add("invalid_input_icon");
+  } else if (username_val.length > 20) {
+    username_error.innerText = "Username cannot be longer than 20 characters";
+    username_error.classList.remove("hidden");
+    errors_count++;
+    username.classList.add("invalid_input");
+    username_icon.classList.add("invalid_input_icon");
+  }
+
+  // username's pattern test, throws error when user gives, special chars on username field
+  const username_pattern = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  if (username_pattern.test(username_val)) {
+    username_error.innerText = "Username can only contain numbers & letters";
+    username_error.classList.remove("hidden");
+    errors_count++;
+    username.classList.add("invalid_input");
+    username_icon.classList.add("invalid_input_icon");
+  }
+
+  // email's length check
+  if (!email_val) {
+    email_error.innerText = "Please enter your email";
+    email_error.classList.remove("hidden");
+    errors_count++;
+    email.classList.add("invalid_input");
+    email_icon.classList.add("invalid_input_icon");
+  } else if (email_val.length > 30) {
+    email_error.innerText = "email cannot be longer than 30 characters";
+    email_error.classList.remove("hidden");
+    errors_count++;
+    email.classList.add("invalid_input");
+    email_icon.classList.add("invalid_input_icon");
+  }
+
+  // email's pattern test, throws error when user gives invalid email on email field
+  const email_pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!email_pattern.test(email_val)) {
+    email_error.innerText = "Please provide a valid email";
+    errors_count++;
+    email_error.classList.remove("hidden");
+    email.classList.add("invalid_input");
+    email_icon.classList.add("invalid_input_icon");
+  }
+
+  // number's length check
+  if (!number_val) {
+    number_error.innerText = "Please enter your number";
+    number_error.classList.remove("hidden");
+    errors_count++;
+    number.classList.add("invalid_input");
+    number_icon.classList.add("invalid_input_icon");
+  } else if (number_val.length !== 10) {
+    number_error.innerText = "Number should be 10 characters long";
+    number_error.classList.remove("hidden");
+    errors_count++;
+    number.classList.add("invalid_input");
+    number_icon.classList.add("invalid_input_icon");
+  }
+
+  // number's pattern test, throws error when user gives invalid number on number field
+  const number_pattern = /[0-9]/;
+  if (!number_pattern.test(number_val)) {
+    number_error.innerText = "Please provide a valid number";
+    number_error.classList.remove("hidden");
+    errors_count++;
+    number.classList.add("invalid_input");
+    number_icon.classList.add("invalid_input_icon");
+  }
+
+  // birthdate's length check
+  if (!birthdate_val) {
+    birthdate_error.innerText = "Please select your birthdate";
+    birthdate_error.classList.remove("hidden");
+    errors_count++;
+    birthdate.classList.add("invalid_input");
+    birthdate_icon.classList.add("invalid_input_icon");
+  }
+
+  // password's length check
+  if (!password_val) {
+    password_error.innerText = "Please enter your password";
+    password_error.classList.remove("hidden");
+    errors_count++;
+    password.classList.add("invalid_input");
+    password_icon.classList.add("invalid_input_icon");
+  } else if (password_val.length < 6) {
+    password_error.innerText = "Password should be longer than 6 character";
+    password_error.classList.remove("hidden");
+    errors_count++;
+    password.classList.add("invalid_input");
+    password_icon.classList.add("invalid_input_icon");
+  } else if (password_val.length > 20) {
+    password_error.innerText = "Password should be less than 20 character";
+    password_error.classList.remove("hidden");
+    errors_count++;
+    password.classList.add("invalid_input");
+    password_icon.classList.add("invalid_input_icon");
+  }
+
+  // password's pattern test, throws error when user gives invalid password on password field
+  const password_pattern = new RegExp(
+    "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})"
+  );
+  if (!password_pattern.test(password_val)) {
+    password_error.innerText =
+      "6 characters long and at least one uppercase, one loweracase, one number and one special character like $,@";
+    password_error.classList.remove("hidden");
+    errors_count++;
+    password.classList.add("invalid_input");
+    password_icon.classList.add("invalid_input_icon");
+  }
+
+  if (errors_count === 0) {
+    form.submit();
+  }
+});
--- public/courses/signup.php
+++ public/courses/signup.php
@@ -16,46 +16,53 @@ if (isset($_SESSION["loggedin"])) {
     <title>User Signup | Pixel IT Magic Solution Courses</title>
     <link rel="stylesheet" href="../assets/styles/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="../assets/js/signup.js" defer></script>
 </head>
 
 <body class="bg-gray-100">
     <div class="min-h-screen flex space-y-8 flex-col justify-center items-center ">
         <h3 class="text-3xl font-bold text-gray-700">Create a new account</h3>
         <p class="text-gray-800">Already have an account? <a href="./login.php" class="underline underline-offset-2">Log In</a> here.</p>
-        <form action="./authenticate.php" method="POST" class="bg-white p-8 space-y-8 rounded-lg">
+        <form action="./authenticate.php" method="POST" class="bg-white p-8 space-y-8 rounded-lg" id="signup_form">
             <div class="flex flex-col sm:flex-row justify-between sm:space-x-4 space-y-6 sm:space-y-0">
                 <div class="flex flex-col-reverse relative">
-                    <i class="fa-solid fa-user absolute top-8 left-3 text-gray-600"></i>
-                    <input autocomplete="name" type="text" name="name" id="name" class="peer mt-4 border-2 border-gray-200 px-4 pl-8 py-2 h-12 rounded-lg" />
+                    <i class="fa-solid fa-user absolute top-8 left-3 text-gray-600" id="name_icon"></i>
+                    <span class="text-red-500 hidden text-xs absolute -bottom-4 left-0 w-96" id="name_error"></span>
+                    <input autocomplete="name" type="text" name="name" id="name" class="peer mt-4 border-2 border-gray-200 px-4 pl-8 py-2 h-12 rounded-lg  w-56" />
                     <label for="name" class="peer-focus:text-lg transition-all block text-md text-gray-700 absolute -top-[10px]">Name</label>
                 </div>
-                <div class="flex flex-col-reverse relative">
-                    <i class="fa-solid fa-envelope absolute top-8 left-3 text-gray-600"></i>
-                    <input autocomplete="Email" type="email" name="Email" id="Email" class="peer mt-4 border-2 border-gray-200 px-4 pl-8 py-2 h-12 rounded-lg" />
+                <div class="flex flex-col-reverse relative ">
+                    <i class="fa-solid fa-envelope absolute top-8 left-3 text-gray-600" id="email_icon"></i>
+                    <span class="text-red-500 hidden text-xs absolute -bottom-4 left-0 w-96" id="email_error"></span>
+                    <input autocomplete="Email" type="email" name="Email" id="email" class="peer mt-4 border-2 border-gray-200 px-4 pl-8 py-2 h-12 rounded-lg  w-56" />
                     <label for="Email" class="peer-focus:text-lg transition-all block text-md text-gray-700 absolute -top-[10px]">Email</label>
                 </div>
             </div>
             <div class="flex flex-col sm:flex-row justify-between sm:space-x-4 space-y-6 sm:space-y-0">
                 <div class="flex flex-col-reverse relative">
-                    <i class="fa-solid fa-mobile-screen-button absolute top-8 left-3 text-gray-600"></i>
-                    <input autocomplete="number" type="tel" name="number" id="number" class="peer mt-4 border-2 border-gray-200 pl-8 px-4 py-2 h-12 rounded-lg" />
+                    <i class="fa-solid fa-mobile-screen-button absolute top-8 left-3 valid_input_icon" id="number_icon"></i>
+                    <span class="text-red-500 hidden text-xs absolute -bottom-4 left-0 w-96" id="number_error"></span>
+                    <input autocomplete="number" type="tel" name="number" id="number" class="peer mt-4 border-2 border-gray-200 pl-8 px-4 py-2 h-12 rounded-lg  w-56" />
                     <label for="number" class="peer-focus:text-lg transition-all block text-md text-gray-700 absolute -top-[10px]">Phone number</label>
                 </div>
                 <div class="flex flex-col-reverse relative">
-                    <i class="fa-solid fa-at absolute top-8 left-3 text-gray-600"></i>
-                    <input autocomplete="username" type="text" name="username" id="username" class="peer mt-4 border-2 border-gray-200 pl-8 px-4 py-2 h-12 rounded-lg" />
+                    <i class="fa-solid fa-at absolute top-8 left-3 valid_input_icon" id="username_icon"></i>
+                    <span class="text-red-500 hidden text-xs absolute -bottom-4 left-0 w-96" id="username_error"></span>
+                    <input autocomplete="username" type="text" name="username" id="username" class="peer mt-4 border-2 border-gray-200 pl-8 px-4 py-2 h-12 rounded-lg  w-56" />
                     <label for="username" class="peer-focus:text-lg transition-all block text-md text-gray-700 absolute -top-[10px]">Username</label>
                 </div>
             </div>
             <div class="flex flex-col sm:flex-row justify-between sm:space-x-4 space-y-6 sm:space-y-0">
                 <div class="flex flex-col-reverse relative">
-                    <i class="fa-solid fa-calendar absolute top-8 left-3 text-gray-600"></i>
-                    <input autocomplete="off" type="date" name="birth-date" id="birth-date" class="peer mt-4 border-2 border-gray-200 pl-8 px-4 py-2 h-12 rounded-lg" />
+                    <i class="fa-solid fa-calendar absolute top-8 left-3 valid_input_icon" id="birth-date_icon"></i>
+                    <span class="text-red-500 hidden text-xs absolute -bottom-4 left-0 w-96" id="birth-date_error"></span>
+                    <input autocomplete="off" type="date" name="birth-date" id="birth-date" class="peer mt-4 border-2 border-gray-200 pl-8 px-4 py-2 h-12 rounded-lg  w-56" />
                     <label for="birth-date" class="peer-focus:text-lg transition-all block text-md text-gray-700 absolute -top-[10px]">Date of birth</label>
                 </div>
                 <div class="flex flex-col-reverse relative">
-                    <i class="fa-solid fa-venus-mars absolute top-8 left-3 text-gray-600"></i>
-                    <select name="gender" id="gender" class="peer mt-4 border-2 border-gray-200 pl-8 px-4 py-2 h-12 rounded-lg">
+                    <i class="fa-solid fa-venus-mars absolute top-8 left-3 valid_input_icon" id="gender_icon"></i>
+                    <span class="text-red-500 hidden text-xs absolute -bottom-4 left-0 w-96" id="gender_error"></span>
+                    <select name="gender" id="gender" class="peer mt-4 border-2 border-gray-200 pl-8 px-4 py-2 h-12 rounded-lg  w-56">
                         <option value="male">Male</option>
                         <option value="female">Female</option>
                         <option value="other">Others</option>
@@ -64,9 +71,10 @@ if (isset($_SESSION["loggedin"])) {
                     <label for="gender" class="peer-focus:text-lg transition-all block text-md text-gray-700 absolute -top-[10px]">Gender</label>
                 </div>
             </div>
-            <div class="flex flex-col-reverse relative">
-                <i class="fa-solid fa-lock absolute top-8 left-3 text-gray-600"></i>
-                <input autocomplete="new-password" type="password" name="password" id="password" class="peer mt-4 border-2 border-gray-200 pl-8 px-4 py-2 h-12 rounded-lg" />
+            <div class="flex flex-col-reverse relative" style="margin-bottom: 60px">
+                <i class="fa-solid fa-lock absolute top-8 left-3 valid_input_icon" id="password_icon"></i>
+                <span class="text-red-500 hidden text-xs absolute -bottom-12 left-0 " id="password_error"></span>
+                <input autocomplete="new-password" type="password" name="password" id="password" class="peer mt-4 border-2 border-gray-200 pl-8 px-4 py-2 h-12 rounded-lg  w-full" />
                 <label for="password" class="peer-focus:text-lg transition-all block text-md text-gray-700 absolute -top-[10px]">Password</label>
             </div>
             <div>
