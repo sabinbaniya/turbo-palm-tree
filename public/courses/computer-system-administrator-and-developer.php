4095d9061b2c6aa02066afdcc44114888fbc231a
sabinbaniya <dummymail@tutanota.de>
2022-06-28 12:34:30 +0545

    computer system admin and dev course

--- public/courses/computer-system-administrator-and-developer.php
+++ public/courses/computer-system-administrator-and-developer.php
@@ -12,7 +12,122 @@
 
 <body>
     <?php require_once("./include/navbar.php") ?>
-
+    <section class="max-w-[1400px] mx-auto px-8 flex flex-col md:flex-row justify-between items-start">
+        <section class="basis-1/2">
+            <section class="my-20 space-y-6 text-gray-600">
+                <h3 class="text-2xl md:text-4xl font-bold text-gray-900">Curriculum Brief</h3>
+                <p>The competency based and market oriented modular curriculum for "Computer system
+                    Administrator and Developer"is designed to produce employable workforce equipped with
+                    knowledge, skills and attitudes related to the occupation.</p>
+                <p>Once the trainees acquired the
+                    competencies they will have ample opportunity for employment through which they will
+                    contribute in the national streamline of poverty reduction in the country. The skills and
+                    knowledge included in this curriculum improve their knowledge and skills and make them
+                    competent Computer system Administrator and Developer needed for the occupation.</p>
+            </section>
+            <section class="my-20 space-y-6 text-gray-600">
+                <h3 class="text-2xl md:text-4xl font-bold text-gray-900">
+                    Aim
+                </h3>
+                <p>
+                    The main aim of this program is to produce employable "Computer system Administrator and
+                    Developer" who could provide different services related to the information technology for
+                    public and private sectors in the country and abroad.
+                </p>
+            </section>
+            <section class="my-20 space-y-6 text-gray-600">
+                <h3 class="text-2xl md:text-4xl font-bold text-gray-900">
+                    Objectives
+                </h3>
+                <p>
+                    After completion of this training, the trainees will be able to implement the knowledge and skills
+                    related to the:
+                </p>
+                <ul style="list-style-type: number; list-style-position: inside;" class="space-y-2">
+                    <li>Communication and Employability Skills for IT</li>
+                    <li>Computer Systems</li>
+                    <li>Information Systems</li>
+                    <li>Impace of the use of IT on Business Systems</li>
+                    <li>Organizational Systems Security</li>
+                    <li>e-Commerce</li>
+                    <li>Managing Networks</li>
+                    <li>Computer Networks</li>
+                    <li>Systems Analysis and Design</li>
+                    <li>Event Driven Programming</li>
+                    <li>Object Oriented Programming</li>
+                    <li>Database Design</li>
+                    <li>Client Side Customization of Web Pages</li>
+                    <li>Data Analysis and Design</li>
+                    <li>Developing Computer Games</li>
+                    <li>Human Computer Interaction</li>
+                    <li>Web Server Scripting</li>
+                    <li>Website Production</li>
+                    <li>Digital Graphics</li>
+                    <li>Computer Animation</li>
+                    <li>Web Animation for Intera</li>
+                </ul>
+            </section>
+            <section class="my-20 space-y-6 text-gray-600">
+                <h3 class="text-2xl md:text-4xl font-bold text-gray-900">
+                    Salient Features
+                </h3>
+                <div>
+                    <div class=" space-y-2">
+                        <p><strong>Duration</strong>:&nbsp;The total duration of the course extends over 1320 hours.&nbsp;</p>
+                        <p><strong>Target group</strong>:&nbsp; The target group for this training program will be all interested individuals with educational prerequisite of minimum SLC pass.<br></p>
+                        <p><strong>Group size</strong>: The group size of this training program will be maximum 24, provided all necessary resources to practice the tasks/ competencies as specified in this curriculum<br></p>
+                        <p><strong>Target location</strong>: The target location of this training program will be all over Nepal.&nbsp;</p>
+                        <p><strong>Medium of Instruction</strong>: The medium of instruction for this training program will be Nepali or English or both.&nbsp;</p>
+                        <p><strong>Pattern of attendance</strong>:&nbsp;Trainee should have 90% attendance during the training period to get the certificate.&nbsp;</p>
+                        <p><strong>Focus of the program</strong>:&nbsp; This is a competency-based modular curriculum. This curriculum emphasizes on competency
+                            performance. 65% time is allotted for performance and remaining 35% time is for related
+                            technical knowledge. So, the main focus will be on performance of the specified competencies in
+                            the curriculum.
+                            &nbsp;</p>
+                    </div>
+                </div>
+            </section>
+            <section class="my-20 space-y-6 text-gray-600">
+                <h3 class="text-2xl md:text-4xl font-bold text-gray-900">
+                    Who is this course meant for
+                </h3>
+                <div>
+                    <div class="space-y-2">
+                        <p><strong>Entry Criteria</strong></p>
+                        <p>Individuals who meet the following criteria will be allowed to enter this curricular program:</p>
+                        <p>Minimum of SLC pass</p>
+                        <p>Nepali citizen</p>
+                        <p>Minimum of 16 years of age</p>
+                        <p>Should pass entrance test</p>
+                    </div>
+                </div>
+            </section>
+            <section class="my-20 space-y-6 text-gray-600">
+                <h3 class="text-2xl md:text-4xl font-bold text-gray-900">
+                    Curricular Structure
+                </h3>
+                <div class="space-y-2">
+                    <h4><a href="../assets/pdf/courses/2019-07-11_Computer System Administrator and Developer 2014.pdf" target="_blank" class="underline">Curriculum of&nbsp;Computer System Administrator and Developer.pdf&nbsp;</a></h4>
+                </div>
+            </section>
+        </section>
+        <section class="my-20 space-y-6 sticky md:top-8 shadow-lg rounded-xl p-8 w-full md:basis-1/3">
+            <h3 class="text-xl md:text-3xl font-bold text-gray-700">
+                Computer System Administrator and Developer Course
+            </h3>
+            <div class="text-gray-600 space-y-4">
+                <p>Credit Hours: 1320 hours</p>
+                <p class="font-bold text-gray-700">Includes : </p>
+                <p>
+                    Fundamentals of IT Course <span class="bg-green-500 rounded-full inline-block px-4 py-1 text-white">Free</span>
+                </p>
+                <p>And all the curiculum listed here</p>
+                <button class="w-full inline-block">
+                    <a href="#" class="bg-blue-500 font-bold text-white text-xl px-4 py-2 w-full inline-block rounded-xl hover:bg-blue-400 focus:ring-4 ring-blue-200">Enroll Now</a>
+                </button>
+            </div>
+        </section>
+    </section>
 
     <?php require_once("./include/footer.php") ?>
 </body>
