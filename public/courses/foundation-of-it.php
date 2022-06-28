30fdb928793a593ebd5267d5455d14e635071d16
sabinbaniya <dummymail@tutanota.de>
2022-06-28 12:58:33 +0545

    foundation of it

--- public/courses/foundation-of-it.php
+++ public/courses/foundation-of-it.php
@@ -12,8 +12,107 @@
 
 <body>
     <?php require_once("./include/navbar.php") ?>
-
-
+    <section class="max-w-[1400px] mx-auto px-8 flex flex-col md:flex-row justify-between items-start">
+        <section class="basis-1/2">
+            <section class="my-20 space-y-6 text-gray-600">
+                <h3 class="text-2xl md:text-4xl font-bold text-gray-900">Curriculum Brief</h3>
+                <p>The competency based and market oriented modular curriculum for "Fundamentals of IT" is designed to produce employable workforce equipped with
+                    knowledge, skills and attitudes related to the occupation.</p>
+                <p>Once the trainees acquired the
+                    competencies they will have ample opportunity for employment through which they will
+                    contribute in the national streamline of poverty reduction in the country. The skills and
+                    knowledge included in this curriculum improve their knowledge and skills and make them
+                    competent Computer system Administrator and Developer needed for the occupation.</p>
+            </section>
+            <section class="my-20 space-y-6 text-gray-600">
+                <h3 class="text-2xl md:text-4xl font-bold text-gray-900">
+                    Aim
+                </h3>
+                <p>
+                    The main aim of this program is to provide further help while learning the "Computer system Administrator and
+                    Developer" course who could provide different services related to the information technology for
+                    public and private sectors in the country and abroad.
+                </p>
+            </section>
+            <section class="my-20 space-y-6 text-gray-600">
+                <h3 class="text-2xl md:text-4xl font-bold text-gray-900">
+                    Objectives
+                </h3>
+                <p>
+                    After completion of this training, the trainees will be able to implement the knowledge and skills
+                    related to the:
+                </p>
+                <ol style="list-style-type: number; list-style-position: inside;" class="space-y-2">
+                    <li>Commputer Fundamentals</li>
+                    <li>Assembling / Dissembling Computer Parts</li>
+                    <li>Opearating System (windows)</li>
+                    <li>Understanding of windows opearting system</li>
+                    <li>Microsoft office
+                        <ul style="list-style-type: disc; margin-left:30px">
+                            <li>Microsoft Word</li>
+                            <li>Microsoft Excel</li>
+                            <li>Microsoft Powerpoint</li>
+                        </ul>
+                    </li>
+                    <li>Fundamentals of Web
+                        <ul style="list-style-type: disc; margin-left:30px">
+                            <li>HTML</li>
+                            <li>DHTML</li>
+                            <li>CSS</li>
+                        </ul>
+                    </li>
+                    <li>Antivirus System, Drivers & Utilities</li>
+                    <li>Email & Internet</li>
+                </ol>
+            </section>
+            <section class="my-20 space-y-6 text-gray-600">
+                <h3 class="text-2xl md:text-4xl font-bold text-gray-900">
+                    Salient Features
+                </h3>
+                <div>
+                    <div class=" space-y-2">
+                        <p><strong>Duration</strong>:&nbsp;The total duration of the course is included in the 1320 hours of the Computer System Administrator and Developer Course.&nbsp;</p>
+                        <p><strong>Target group</strong>:&nbsp; The target group for this training program will be all interested individuals with educational prerequisite of minimum SLC pass.<br></p>
+                        <p><strong>Group size</strong>: The group size of this training program will be maximum 24, provided all necessary resources to practice the tasks/ competencies as specified in this curriculum<br></p>
+                        <p><strong>Target location</strong>: The target location of this training program will be all over Nepal.&nbsp;</p>
+                        <p><strong>Medium of Instruction</strong>: The medium of instruction for this training program will be Nepali or English or both.&nbsp;</p>
+                        <p><strong>Pattern of attendance</strong>:&nbsp;Trainee should have 90% attendance during the training period to get the certificate.&nbsp;</p>
+                        <p><strong>Focus of the program</strong>:&nbsp; This is a competency-based modular curriculum. This curriculum emphasizes on competency
+                            performance. 65% time is allotted for performance and remaining 35% time is for related
+                            technical knowledge. So, the main focus will be on performance of the specified competencies in
+                            the curriculum.
+                            &nbsp;</p>
+                    </div>
+                </div>
+            </section>
+            <section class="my-20 space-y-6 text-gray-600">
+                <h3 class="text-2xl md:text-4xl font-bold text-gray-900">
+                    Who is this course meant for
+                </h3>
+                <div>
+                    <div class="space-y-2">
+                        <p><strong>Entry Criteria</strong></p>
+                        <p>Individuals who meet the following criteria will be allowed to enter this curricular program:</p>
+                        <p>Minimum of SEE/SLC pass</p>
+                        <p>Nepali citizen</p>
+                        <p>Minimum of 16 years of age</p>
+                    </div>
+                </div>
+            </section>
+        </section>
+        <section class="my-20 space-y-6 sticky md:top-8 shadow-lg rounded-xl p-8 w-full md:basis-1/3">
+            <h3 class="text-xl md:text-3xl font-bold text-gray-700">
+                Foundation of IT Course
+            </h3>
+            <div class="text-gray-600 space-y-4">
+                <p class="font-bold text-gray-700">Includes : </p>
+                <p>All the curiculum listed here</p>
+                <button class="w-full inline-block">
+                    <a href="#" class="bg-blue-500 font-bold text-white text-xl px-4 py-2 w-full inline-block rounded-xl hover:bg-blue-400 focus:ring-4 ring-blue-200">Enroll Now</a>
+                </button>
+            </div>
+        </section>
+    </section>
     <?php require_once("./include/footer.php") ?>
 </body>
 
